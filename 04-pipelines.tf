f54dcf82d85e6ead0aa567086a90e9e359e59294
root <cristiano.rosetti@gmail.com>
2024-04-25 03:39:22 +0000

    idp

--- 04-pipelines.tf
+++ 04-pipelines.tf
@@ -5,17 +5,12 @@ resource "harness_platform_pipeline" "pipeline" {
   name       = "Full Deployment Kubernetes"
 
   yaml = templatefile("./pipelines/full-deploymet.tftpl", {
-    identifier           = "pipeline_cicd"
-    name                 = "Pipeline Kubernetes Java"
-    org_id               = var.org_id
-    project_id           = harness_platform_project.this.identifier
-    github_connector     = "account.${harness_platform_connector_github.this.identifier}"
-    github_repo          = "harness-cie-lab"
-    build_infrastructure = "account.${harness_platform_connector_kubernetes.build.identifier}"
-    gcp_connector        = "account.${harness_platform_connector_gcp.this.identifier}"
-    gcs_bucket           = "crizstian-terraform"
-    docker_connector     = "account.${harness_platform_connector_docker.this.identifier}"
-    service_id           = harness_platform_service.this.identifier
-    jira_connector       = "account.${harness_platform_connector_jira.this.identifier}"
+    identifier  = "pipeline_cicd"
+    name        = "Pipeline Kubernetes Java"
+    org_id      = var.org_id
+    project_id  = harness_platform_project.this.identifier
+    github_repo = "harness-cie-lab"
+    gcs_bucket  = "crizstian-terraform"
+    service_id  = harness_platform_service.this.identifier
   })
 }
