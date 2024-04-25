8a2c2b6c274029c2421d1b5325b47706e316c022
root <cristiano.rosetti@gmail.com>
2024-04-25 03:53:28 +0000

    idp

--- 03-service.tf
+++ 03-service.tf
@@ -16,64 +16,39 @@ service:
   identifier: ${replace(var.service_name, "-", "_")}
   orgIdentifier: ${var.org_id}
   projectIdentifier: ${harness_platform_project.this.identifier}
+  gitOpsEnabled: false
   serviceDefinition:
+    type: Kubernetes
     spec:
-      variables:
-        - name: harness_service
-          type: String
-          description: ""
-          required: false
-          value: "<+service.identifier>"
-        - name: environment
-          type: String
-          description: ""
-          required: false
-          value: " <+env.name>"
-        - name: image_tag
-          type: String
-          description: ""
-          required: false
-          value: " <+pipeline.stages.Build_Test_Push.spec.execution.steps.Create_Tag.output.outputVariables.TAG>"
+      manifests:
+        - manifest:
+            identifier: manifiesto
+            type: K8sManifest
+            spec:
+              store:
+                type: Github
+                spec:
+                  connectorRef: account.Cristian_V2_Org
+                  gitFetchType: Branch
+                  paths:
+                    - deploy
+                  repoName: ${var.service_name}
+                  branch: main
+              valuesPaths: <+input>
+              skipResourceVersioning: false
+              enableDeclarativeRollback: false
       artifacts:
         primary:
           primaryArtifactRef: <+input>
           sources:
             - spec:
                 connectorRef: account.cristian_docker
-                imagePath: crizstian/guestbook
-                tag: latest
+                imagePath: crizstian/payment-service-workshop
+                tag: <+input>
                 digest: ""
-              identifier: guestbook
+              identifier: Docker
               type: DockerRegistry
-      manifests:
-        - manifest:
-            identifier: config
-            type: ReleaseRepo
-            spec:
-              store:
-                type: Github
-                spec:
-                  connectorRef: account.cristian_github
-                  gitFetchType: Branch
-                  paths:
-                    - cluster-config/<+cluster.name>/config.json
-                  branch: main
-                  repoName: guestbook-delivery
-        - manifest:
-            identifier: appset
-            type: DeploymentRepo
-            spec:
-              store:
-                type: Github
-                spec:
-                  connectorRef: account.cristian_github
-                  gitFetchType: Branch
-                  paths:
-                    - appset/applicationset.yaml
-                  branch: main
-                  repoName: guestbook-delivery
-    type: Kubernetes
-  gitOpsEnabled: true
+
 
     EOT
 }
